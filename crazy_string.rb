54208eaa4e06d56d8a6980d699c4b8810f8f51c8
Aarushi Machavarapu <aarushimac@gmail.com>
2018-07-10 14:34:39 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,8 +1,8 @@
 # write your method here
 
 def crazy_strings(a,b)
- a=a.reverse.upcase
- b=b.swapcase.gsub("s","z")
+ a == a.reverse.upcase
+ b == b.swapcase.gsub("s","z")
  return "#{a}#{b}"
 end
 
